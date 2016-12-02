02eaada2376501eec72a1fcee09f26d8520b534c
Chris Antaki <ChrisAntaki@gmail.com>
2016-12-02 11:49:46 -0800

    Updating email

--- js/constants.js
+++ js/constants.js
@@ -9,13 +9,29 @@ constants.domain = 'fixtheelectoralcollege.net';
 
 // Social
 constants.emailSubject = 'Sign this petition: Fix the Electoral College';
-constants.emailBody = `Hi,
+constants.emailBody = `Friends-
 
-I just signed a petition at FixTheElectoralCollege.net to end the outdated, anti-democratic Electoral College's power to overturn the popular vote.
+Every vote should count equally, regardless of where you live in the United States – EVERY vote in EVERY state should matter in EVERY presidential election. But thanks to the Electoral College, even though Clinton won over 2 million (and counting!) more popular votes, the Electoral College allowed Trump to win.
 
-Will you take a moment to support the National Popular Vote Interstate Compact?
+Visit ${constants.domain} to join me in supporting the National Popular Vote compact, which would ensure that the popular vote winner becomes the president.
 
-https://${constants.domain}/?source=${StaticKit.query.cleanedSource}-emailshare
+This is the fifth time in American history, and second time in five elections, where a presidential candidate who won the most popular votes lost the election. As a result, presidential candidates have no reason to pay attention to voters in states where they are safely ahead or hopelessly behind. Instead, candidates focus on “swing states” while largely ignoring the rest of the country.
+
+The winner-take-all Electoral College system that led to this anti-democratic outcome must be changed, so that voters in all 50 states have a say in choosing our president.
+
+If we can get enough states to sign onto the National Popular Vote Interstate Compact, our presidents will be elected by the national popular vote — it would fix the problems of the Electoral College without needing to amend the Constitution or go through Congress.
+
+The National Popular Vote Interstate Compact would require states to award all their electoral votes to whichever presidential candidate wins the national popular vote.
+
+Visit ${constants.domain} to join me in supporting the National Popular Vote compact, which would ensure that the popular vote winner becomes the president.
+
+And we already have momentum to getting this enacted nationally: Right now, ten states and the District of Columbia have joined the compact, possessing 165 electoral votes altogether.
+
+It wouldn’t take effect until enough states joined in, but we’re closer to that than you might think — ten states and the District of Columbia have already signed on, totaling 165 electoral votes of the needed 270.
+
+Activating the National Popular Vote Compact would reshape our democracy for the better. Not only would it ensure that the person who actually got more votes win the presidency, but it would also force candidates to spend time engaging with voters in all 50 states, instead of just a handful swing states.
+
+Visit ${constants.domain} to join me in supporting the National Popular Vote compact, which would ensure that the popular vote winner becomes the president.
 
 Thanks!`;
 constants.tweet = `Fix the Electoral College: Whoever gets the most popular votes should become president! https://fixtheelectoralcollege.com`;
